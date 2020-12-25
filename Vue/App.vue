5596b1c072e20f90f2a30c85f04eb6246151dafb
Yuriy Lyukov <lyukov.yura@gmail.com>
2020-12-25 19:57:10 +0200

    SVG path animation

--- Vue/App.vue
+++ Vue/App.vue
@@ -3,6 +3,7 @@
     <NavBar></NavBar>
     <Counter></Counter>
     <HowItWorks></HowItWorks>
+    <SVGpath></SVGpath>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import NavBar from "./components/NavBar.vue";
 import Counter from "./components/Counter";
 import HowItWorks from "./components/HowItWorks";
+import SVGpath from "./components/SVGpath";
 
 export default {
   name: "App",
   components:{
     NavBar,
     Counter,
-    HowItWorks
+    HowItWorks,
+    SVGpath
   }
 }
 </script>
--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "../Vue/App.vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import "scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap";
+import "scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators";
 
 Vue.directive ('scroll', {
     inserted: function (el, binding) {
